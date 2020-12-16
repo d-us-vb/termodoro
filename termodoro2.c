6301fb6ae5e4f8d97a3f424aa6fc5b4088a507b9
Jeremiah Glover <jeremiah7glover@gmail.com>
2020-12-16 12:58:21 -0600

    add log config ds; add more fields and documentation

--- termodoro2.c
+++ termodoro2.c
@@ -7,60 +7,90 @@
 #include <pthread.h>
 #include <sys/stat.h>
 
-#define CTRL_KEY(k) ((k) & 0x1f)
-#define CRLF "\r\n"
-#define ABUF_INIT {NULL, 0}
+/************************* GLOBALS ************************/
+// we use const variables  rather than compiler macros for type safety.
 
 // display line font
-#define FONT_COLON_INDEX 10
-#define FONT_DECPT_INDEX 11
+const int FONT_COLON_INDEX = 10;
+const int FONT_DECPT_INDEX = 11;
 
-#define INPUT_QUEUE_SIZE 10
+// input queue
+const int  INPUT_QUEUE_SIZE = 10;
 
 
 // display constants
-#define DISPLAY_LINE_LENGTH 80
-#define COMMAND_LINE_LENGTH 80
+const int DISPLAY_LINE_LENGTH = 80;
+const int COMMAND_LINE_LENGTH = 80;
 
-#define WINDOW_HEIGHT 24
+const int WINDOW_HEIGHT = 24;
 
 // string constants for configuration and command interpreting
 
 const char POMODORO_LENGTH_SECONDS_STRING[] = "pomodoro_length_seconds";
+
 const char POMODORO_SHORT_BREAK_LENGTH_SECONDS_STRING[] =
    "pomodoro_short_break_length_seconds";
+
 const char POMODORO_LONG_BREAK_LENGTH_SECONDS_STRING[] =
    "pomodoro_long_break_length_seconds";
+
 const char POMODORO_SET_LENGTH_STRING[] = "pomodoro_set_length";
+
 const char POMODORO_DEFAULT_SESSION_GOAL_STRING[] =
    "pomodoro_default_session_goal";
 
 const char CONTINUE_TRACKING_TIME_UPON_COMPLETION[] =
    "continue_tracking_time_upon_completion";
+
 const char AUTO_START_SHORT_BREAKS_STRING[] = "auto_start_short_breaks";
+
 const char AUTO_START_LONG_BREAKS_STRING[] = "auto_start_long_breaks";
 
 const char SHOW_BIG_TIME_STRING[] = "show_big_time";
+
 const char SHOW_SMALL_TIME_STRING[] = "show_small_time";
+
 const char SHOW_TIME_LOGGED_SO_FAR_STRING[] = "show_time_logged_so_far";
+
 const char SHOW_TIME_LEFT_STRING[] = "show_time_left";
 
 const char EXIT_SHORTCUT_STRING[] = "exit_shortcut";
+
 const char BEGIN_POMODORO_SHORTCUT_STRING[] = "begin_pomodoro_shortcut";
 
 const char BEGIN_SHORT_BREAK_SHORTCUT_STRING[] = "begin_short_break_shortcut";
+
 const char BEGIN_LONG_BREAK_SHORTCUT_STRING[] = "begin_long_break_shortcut";
+
 const char ENTER_COMMAND_SHORTCUT_STRING[] = "enter_command_shortcut";
+
 const char PREVIOUS_CHAR_SHORTCUT_STRING[] = "previous_char_shortcut";
+
 const char NEXT_CHAR_SHORTCUT_STRING[] = "next_char_shortcut";
+
 const char VIEW_HELP_DOCUMENT_SHORTCUT_STRING[] = "view_help_document_shortcut";
+
 const char INTERRUPT_CURRENT_ACTIVITY_STRING[] =
    "interrupt_current_activity_shortcut";
 
 const char ALERT_WITH_AUDIO_STRING[] = "view_help_document_shortcut";
+
 const char ALERT_AUDIO_FILE_STRING[] = "alert_audio_file";
 
+const char DEFAULT_CONFIG_FILE_PATH[] = "../config/.termodoro";
+
+const char DEFAULT_STATUS_LOG_FILE_PATH[] = "../logs/termodoro_log.txt";
+
 /******************** DATA STRUCTURES *********************/
+
+typedef struct StatusLogConfigContainer
+{
+  // contains the file path, if different from the default.
+  char* status_log_file_path;
+
+  // may hold 0, 1, 2, 3
+  int verbosity_level;
+} StatusLogConfigContainer;
 
 // AppConfigurationContainer
 //
@@ -139,6 +169,21 @@ AppStateContainer* app_state;
 
 /*********************** INITIALIZATION *******************/
 
+/*** InitStatusLog ***
+ *
+ * The Status log keeps track of what the app is doing. There are three
+ * verbosity levels of logging:
+ * 1. Standard: only main events are logged.
+ * 2. Verbose: all events are logged.
+ * 3. Debug: all events are logged and the entire program state is
+ *    recorded as well.
+ */
+void InitStatusLog() {
+  
+
+  return;
+}
+
 /*** LoadBigFont ***
  *
  * this takes the name of a font file as a c string and loads it into the
@@ -168,14 +213,15 @@ int LoadTermodoroConfigFile(char* file_name)
 
    return 0;
 }
-/*** InitializeAppConfig ***
+
+/*** InitAppConfig ***
  *
  * This function looks for a .termodoro file for app configuration. If there
  * isn't one, it initializes the app_config global with the defaults given
  * in the user manual. (Section Default Configuration).
  *
  */
-void InitalizeAppConfig()
+void InitAppConfig()
 {
    // attempt loading from
    return;
